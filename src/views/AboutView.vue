3f5721dd3787747aaa91c3b2fb419415c4018b83
= <l.piccand@protonmail.ch>
2023-03-03 12:04:45 +0100

    About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,9 +2,9 @@
   <div class="about">
     <h1>About Metal Archiver</h1>
     <p>This is a demo made with Vue.js and the custom made OppidumWeb API, which displays data about metal bands in a mobile-friendly interface.</p>
-    <p>The data comes from Encyclopaedia Metallum (Metal-Archives).</p>
-    <p>This was done for educational purposes, there is no intention of stealing data or making heavy requests.</p>
+    <p>The data comes from Encyclopaedia Metallum, also known as <a href="https://www.metal-archives.com" target="_blank">Metal-Archives</a>.</p>
+    <p>This was done for educational purposes, there is no intention of stealing data etc. A local cache is used as much as possible to prevent heavy requests.</p>
 
-    <p>Metal Archiver is made by <a href="https://www.oppidumweb.net" target="_blank">Lucius Barde</a>.</p>
+    <p>Metal Archiver was made by <a href="https://www.oppidumweb.net" target="_blank">Lucius Barde</a> in 2022-2023.</p>
   </div>
 </template>
